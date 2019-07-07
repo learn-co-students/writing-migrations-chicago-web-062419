97d5ed8f3f6b1e6e3367548ffa1eb02f02496ec6
Amanda Treutler <amandathedev@gmail.com>
2019-07-07 12:21:17 -0500

    Done.

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -1,2 +1,12 @@
 class CreateStudents < ActiveRecord::Migration[5.1]
+  def change
+    create_table :students do |t|
+      t.string :name
+    end
+  end
 end
+
+# ActiveRecord::Base.establish_connection(
+#   :adapter => "sqlite3",
+#   :database => "db/artists.sqlite",
+# )
